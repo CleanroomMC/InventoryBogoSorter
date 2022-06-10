2987ade9af872c0fef893e3688a838904f1bf694
brachy84 <brachy84@gmail.com>
2022-06-10 13:42:06 +0200

    double chest support

--- src/main/java/com/cleanroommc/invtweaks/compat/DefaultCompat.java
+++ src/main/java/com/cleanroommc/invtweaks/compat/DefaultCompat.java
@@ -2,12 +2,14 @@ package com.cleanroommc.invtweaks.compat;
 
 import com.cleanroommc.invtweaks.api.InventoryTweaksAPI;
 import net.minecraft.inventory.ContainerChest;
+import net.minecraft.inventory.IInventory;
 
 public class DefaultCompat {
 
     public static void init() {
         InventoryTweaksAPI.addCompat(ContainerChest.class, (container, builder) -> {
-            builder.addSlotGroup(9, 0, 27);
+            IInventory inventory = container.getLowerChestInventory();
+            builder.addSlotGroup(9, 0, inventory.getSizeInventory());
         });
     }
 }
