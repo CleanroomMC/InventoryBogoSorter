7c17a8651ab9a978a1b770309ed3546ea8a5d5f7
brachy84 <brachy84@gmail.com>
2022-06-18 19:08:50 +0200

    aa support

--- src/main/java/com/cleanroommc/bogosorter/compat/DefaultCompat.java
+++ src/main/java/com/cleanroommc/bogosorter/compat/DefaultCompat.java
@@ -1,6 +1,7 @@
 package com.cleanroommc.bogosorter.compat;
 
 import com.cleanroommc.bogosorter.api.IBogoSortAPI;
+import de.ellpeck.actuallyadditions.mod.inventory.ContainerGiantChest;
 import net.minecraft.inventory.*;
 
 public class DefaultCompat {
@@ -21,5 +22,9 @@ public class DefaultCompat {
             builder.addSlotGroup(9, 0, 27);
         });
         // for horse inventory compat see MixinContainerHorseInventory
+
+        api.addCompat(ContainerGiantChest.class, (container, builder) -> {
+            builder.addSlotGroup(13, 0, 117);
+        });
     }
 }
