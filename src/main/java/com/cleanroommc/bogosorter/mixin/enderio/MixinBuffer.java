c02be9cbc3f2949a181a28d7513df23ed84f500b
brachy84 <brachy84@gmail.com>
2022-06-19 11:59:13 +0200

    ender io buffer & vacuum chest compat

---
+++ src/main/java/com/cleanroommc/bogosorter/mixin/enderio/MixinBuffer.java
@@ -0,0 +1,18 @@
+package com.cleanroommc.bogosorter.mixin.enderio;
+
+import com.cleanroommc.bogosorter.api.ISortableContainer;
+import com.cleanroommc.bogosorter.api.ISortingContextBuilder;
+import crazypants.enderio.machines.machine.buffer.ContainerBuffer;
+import net.minecraft.inventory.Container;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ContainerBuffer.class)
+public class MixinBuffer implements ISortableContainer {
+
+    @Override
+    public void buildSortingContext(ISortingContextBuilder builder) {
+        if (((Container) (Object) this).inventorySlots.size() > 36) {
+            builder.addSlotGroup(3, 0, 9);
+        }
+    }
+}
---
+++ src/main/java/com/cleanroommc/bogosorter/mixin/enderio/MixinVacuumChest.java
@@ -0,0 +1,15 @@
+package com.cleanroommc.bogosorter.mixin.enderio;
+
+import com.cleanroommc.bogosorter.api.ISortableContainer;
+import com.cleanroommc.bogosorter.api.ISortingContextBuilder;
+import crazypants.enderio.machines.machine.vacuum.chest.ContainerVacuumChest;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(ContainerVacuumChest.class)
+public class MixinVacuumChest implements ISortableContainer {
+
+    @Override
+    public void buildSortingContext(ISortingContextBuilder builder) {
+        builder.addSlotGroup(9, 1, 28);
+    }
+}
