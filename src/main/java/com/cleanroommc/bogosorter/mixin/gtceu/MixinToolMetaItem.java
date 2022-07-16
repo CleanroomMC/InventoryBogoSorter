8534fdb05db41a5d3e0234de7943ccf46aa0271d
brachy84 <brachy84@gmail.com>
2022-07-16 18:01:15 +0200

    fix gtceu mixin

--- src/main/java/com/cleanroommc/bogosorter/mixin/gtceu/MixinToolMetaItem.java
+++ src/main/java/com/cleanroommc/bogosorter/mixin/gtceu/MixinToolMetaItem.java
@@ -2,7 +2,6 @@ package com.cleanroommc.bogosorter.mixin.gtceu;
 
 import com.cleanroommc.bogosorter.BogoSorterConfig;
 import com.cleanroommc.bogosorter.common.refill.RefillHandler;
-import gregtech.api.items.IToolItem;
 import gregtech.api.items.toolitem.ToolMetaItem;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
@@ -13,28 +12,26 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(ToolMetaItem.class)
-public abstract class MixinToolMetaItem
-{
+@Mixin(value = ToolMetaItem.class, remap = false)
+public abstract class MixinToolMetaItem {
+
     @Shadow
     public abstract int getItemDamage(ItemStack itemStack);
 
     @Shadow
     public abstract int getMaxItemDamage(ItemStack itemStack);
 
-    @Inject(method = "damageItem", at = @At("RETURN"), remap = false)
-    private void damageItem(ItemStack stack, EntityLivingBase entity, int vanillaDamage, boolean allowPartial, boolean simulate, CallbackInfoReturnable<Integer> cir){
-        if (simulate) return;
-        if (!(entity instanceof EntityPlayer)) return;
+    @Inject(method = "damageItem", at = @At(value = "INVOKE", target = "Ljava/lang/Math;min(II)I"))
+    private void damageItem(ItemStack stack, EntityLivingBase entity, int vanillaDamage, boolean allowPartial, boolean simulate, CallbackInfoReturnable<Integer> cir) {
+        if (!simulate && entity instanceof EntityPlayer) {
+            EntityPlayer player = ((EntityPlayer) entity);
 
-        EntityPlayer player = ((EntityPlayer) entity);
-
-        if (RefillHandler.shouldHandleRefill(player, stack)) {
-            int durabilityLeft = getMaxItemDamage(stack) - getItemDamage(stack);
-            if (durabilityLeft >= 0 && durabilityLeft < BogoSorterConfig.autoRefillDamageThreshold) {
-                new RefillHandler(player.inventory.currentItem, stack, player, true).handleRefill();
+            if (RefillHandler.shouldHandleRefill(player, stack) && stack.hasTagCompound() && stack.getTagCompound().hasKey("GT.ToolStats")) {
+                int durabilityLeft = getMaxItemDamage(stack) - getItemDamage(stack);
+                if (durabilityLeft > 0 && durabilityLeft <= BogoSorterConfig.autoRefillDamageThreshold) {
+                    new RefillHandler(player.inventory.currentItem, stack, player, true).handleRefill();
+                }
             }
         }
     }
-
 }
