527ddf2db48a7473ad066f62900cb6d0e6615133
Caedis <Caedis@users.noreply.github.com>
2022-07-16 23:18:24 -0500

    Finish up items that use onItemUseFinish

--- src/main/java/com/cleanroommc/bogosorter/mixin/MixinEntityLivingBase.java
+++ src/main/java/com/cleanroommc/bogosorter/mixin/MixinEntityLivingBase.java
@@ -1,13 +1,11 @@
 package com.cleanroommc.bogosorter.mixin;
 
 import com.cleanroommc.bogosorter.BogoSorter;
-import com.cleanroommc.bogosorter.BogoSorterConfig;
 import com.cleanroommc.bogosorter.common.refill.RefillHandler;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,7 +17,6 @@ public abstract class MixinEntityLivingBase
     /**
      *  activeItemStackCopy = what was used
      *  itemstack = the item's onItemUseFinish return item (usually empty, but can return a bowl, bottle, etc)
-     *  this.activeItemStack = not empty when there is an item in active slot. Used to detect when to refill
      */
     @Inject(method = "onItemUseFinish",
             at = @At(
@@ -33,11 +30,17 @@ public abstract class MixinEntityLivingBase
         if (!(getThis() instanceof EntityPlayer)) return;
         EntityPlayer player = (EntityPlayer) getThis();
 
+        //  used in cases where a modded item returns itself with a different durability (AA coffee, Botania Vials, etc)
+        if (ItemStack.areItemsEqualIgnoreDurability(activeItemStackCopy, itemstack)){
+            return;
+        }
+
         if (RefillHandler.shouldHandleRefill(player, activeItemStackCopy)) {
-            new RefillHandler(player.inventory.currentItem, activeItemStackCopy, player).handleRefill();
-            if (!itemstack.isEmpty()){
-                if (!player.inventory.addItemStackToInventory(itemstack)){
-                    player.dropItem(itemstack, true);
+            boolean didSwap = new RefillHandler(player.inventory.currentItem, activeItemStackCopy, player).handleRefill();
+            if (didSwap && !itemstack.isEmpty()){
+                if (!player.inventory.addItemStackToInventory(itemstack)) {
+                    BogoSorter.LOGGER.info("Dropping item that does not fit");
+                    player.dropItem(itemstack, true, false);
                 }
             }
         }
