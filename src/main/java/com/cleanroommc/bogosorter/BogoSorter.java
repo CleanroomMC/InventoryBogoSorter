7dc59e8d479b674d6ab4ceed288820fb9cb43f8a
brachy84 <brachy84@gmail.com>
2022-07-24 07:32:29 +0200

    1.0.5

--- src/main/java/com/cleanroommc/bogosorter/BogoSorter.java
+++ src/main/java/com/cleanroommc/bogosorter/BogoSorter.java
@@ -22,13 +22,13 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = BogoSorter.ID, name = BogoSorter.NAME, version = BogoSorter.VERSION, dependencies = "required-after:modularui@[1.0.5,);required-after:mixinbooter@[5.0,)")
+@Mod(modid = BogoSorter.ID, name = BogoSorter.NAME, version = BogoSorter.VERSION, dependencies = "required-after:modularui@[1.0.6,);required-after:mixinbooter@[4.2,)")
 @Mod.EventBusSubscriber(modid = BogoSorter.ID)
 public class BogoSorter {
 
     public static final String ID = "bogosorter";
     public static final String NAME = "Inventory Bogo Sorter";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
 
     public static final Logger LOGGER = LogManager.getLogger(ID);
 
