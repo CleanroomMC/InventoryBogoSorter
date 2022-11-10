6b24a09752722d11a7e02bd725e789c92ea92ebb
brachy84 <brachy84@gmail.com>
2022-11-10 18:15:47 +0100

    1.1.2

--- src/main/java/com/cleanroommc/bogosorter/BogoSorter.java
+++ src/main/java/com/cleanroommc/bogosorter/BogoSorter.java
@@ -29,7 +29,7 @@ public class BogoSorter {
 
     public static final String ID = "bogosorter";
     public static final String NAME = "Inventory Bogo Sorter";
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.1.2";
 
     public static final Logger LOGGER = LogManager.getLogger(ID);
 
