7fca491316ed1ec9824a7e0dd46951b45122d0e1
ZZZank <3410764033@qq.com>
2025-05-20 09:34:47 +0800

    optimize MappedSlotHandler

--- src/main/java/com/cleanroommc/bogosorter/compat/data_driven/handler/MappedSlotHandler.java
+++ src/main/java/com/cleanroommc/bogosorter/compat/data_driven/handler/MappedSlotHandler.java
@@ -6,17 +6,18 @@ import com.cleanroommc.bogosorter.api.ISlot;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.Slot;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
 
 /**
  * @author ZZZank
  */
 public class MappedSlotHandler extends HandlerBase {
     private final int rowSize;
-    private final List<Predicate<Slot>> filters;
+    private final Predicate<Slot> filter;
     private final Function<Slot, ISlot> reducer;
 
     public MappedSlotHandler(
@@ -27,7 +28,7 @@ public class MappedSlotHandler extends HandlerBase {
     ) {
         super(target);
         this.rowSize = rowSize;
-        this.filters = filters;
+        this.filter = andCompressed(filters);
         this.reducer = reducer;
         BogoSorter.LOGGER.info(
             "constructed mapped bogo compat handler targeting '{}' with row size '{}'",
@@ -36,16 +37,42 @@ public class MappedSlotHandler extends HandlerBase {
         );
     }
 
+    private static Predicate<Slot> andCompressed(Collection<? extends Predicate<Slot>> filters) {
+        var iter = filters.iterator();
+        switch (filters.size()) {
+            case 0:
+                return (slot) -> true;
+            case 1:
+                return iter.next();
+            case 2:
+                return iter.next().and(iter.next());
+            case 3:
+                Predicate<Slot> pred1 = iter.next(), pred2 = iter.next(), pred3 = iter.next();
+                return slot -> pred1.test(slot) && pred2.test(slot) && pred3.test(slot);
+        }
+        var predicates =(Predicate<Slot>[]) filters.toArray(new Predicate[0]);
+        return slot -> {
+            for (var predicate : predicates) {
+                if (!predicate.test(slot)) {
+                    return false;
+                }
+            }
+            return true;
+        };
+    }
+
     @Override
     public void handle(IBogoSortAPI api) {
         api.addCompat(
             target,
             (container, builder) -> {
-                var s = container.inventorySlots.stream();
-                for (var filter : filters) {
-                    s = s.filter(filter);
+                var slots = new ArrayList<ISlot>();
+                for (var slot : container.inventorySlots) {
+                    if (filter.test(slot)) {
+                        slots.add(reducer.apply(slot));
+                    }
                 }
-                builder.addSlotGroup(s.map(reducer).collect(Collectors.toList()), rowSize);
+                builder.addSlotGroup(slots, rowSize);
             }
         );
     }
