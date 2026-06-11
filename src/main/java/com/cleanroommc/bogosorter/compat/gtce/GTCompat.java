51a676c7bfbb4413737ad39345010bc25596b778
Exa <11907282+Exaxxion@users.noreply.github.com>
2026-06-11 00:33:31 -0700

    Neuron activation
    Should have just made this a property of the GT record to begin with

--- src/main/java/com/cleanroommc/bogosorter/compat/gtce/GTCompat.java
+++ src/main/java/com/cleanroommc/bogosorter/compat/gtce/GTCompat.java
@@ -16,21 +16,21 @@ public class GTCompat {
     private static final GT gt;
 
     @Desugar
-    private record GT(String toolClassName, String statsTag, String materialKey) {
-        GT(String toolClassName, String statsTag) {
-            this(toolClassName, statsTag, "Material");
+    private record GT(String modName, String toolClassName, String statsTag, String materialKey) {
+        GT(String modName, String toolClassName, String statsTag) {
+            this(modName, toolClassName, statsTag, "Material");
         }
-        GT() {
-            this("gregtech.common.items.MetaTool", "GT.ToolStats");
+        GT(String modName) {
+            this(modName, "gregtech.common.items.MetaTool", "GT.ToolStats");
         }
     }
 
     /** GregTech Community Edition */
-    private static final GT GTCE  = new GT();
+    private static final GT GTCE  = new GT("GregTech Community Edition");
     /** GregTech Nomifactory Edition */
-    private static final GT GTNE  = new GT();
+    private static final GT GTNE  = new GT("GregTech Nomifactory Edition");
     /** GregTech Community Edition Unofficial */
-    private static final GT GTCEu = new GT("gregtech.api.items.toolitem.IGTTool", "GT.Tool");
+    private static final GT GTCEu = new GT("GregTech", "gregtech.api.items.toolitem.IGTTool", "GT.Tool");
 
     static {
         // All three GregTechs use the ModID "gregtech"
@@ -81,14 +81,14 @@ public class GTCompat {
     }
 
     public static boolean isGTCEu(ModContainer m) {
-        return "GregTech".equals(m.getMetadata().name);
+        return GTCEu.modName.equals(m.getMetadata().name);
     }
 
     public static boolean isGTCE(ModContainer m) {
-        return "GregTech Community Edition".equals(m.getMetadata().name);
+        return GTCE.modName.equals(m.getMetadata().name);
     }
 
     public static boolean isGTNE(ModContainer m) {
-        return "GregTech Nomifactory Edition".equals(m.getMetadata().name);
+        return GTNE.modName.equals(m.getMetadata().name);
     }
 }
