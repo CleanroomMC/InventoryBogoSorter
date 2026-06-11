4a463b7e0c90299f5ea4da463d7b9538c127d63f
Exa <11907282+Exaxxion@users.noreply.github.com>
2026-06-11 00:16:43 -0700

    Remove reflection-based version detection, use ModContainer instead

--- src/main/java/com/cleanroommc/bogosorter/compat/gtce/GTCompat.java
+++ src/main/java/com/cleanroommc/bogosorter/compat/gtce/GTCompat.java
@@ -1,15 +1,15 @@
 package com.cleanroommc.bogosorter.compat.gtce;
 
+import com.cleanroommc.bogosorter.BogoSorter;
 import com.github.bsideup.jabel.Desugar;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.ModContainer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class GTCompat {
-    @Nullable
-    private static final Class<?> versionClass;
     @Nullable
     private static final Class<?> toolClass;
     @Nullable
@@ -33,44 +33,30 @@ public class GTCompat {
     private static final GT GTCEu = new GT("gregtech.api.items.toolitem.IGTTool", "GT.Tool");
 
     static {
-        Class<?> clazz;
-
-        // dynamically load the version info, otherwise it's a constant recorded at compile time
-        try {
-            clazz = Class.forName("gregtech.GregTechVersion", false, GTCompat.class.getClassLoader());
-        } catch(Exception ignored) {
-            clazz = null;
-        }
-        versionClass = clazz;
+        // All three GregTechs use the ModID "gregtech"
+        BogoSorter.Mods mod = BogoSorter.Mods.GT_ANY;
 
         // Determine which GregTech is loaded
-        if(versionClass == null)
+        // no GT loaded
+        if(Loader.isModLoaded(mod.id)) {
+            ModContainer m = Loader.instance().getIndexedModList().get(mod.id);
+            if(isGTCE(m))
+                gt = GTCE;
+            else if(isGTNE(m))
+                gt = GTNE;
+            else if(isGTCEu(m))
+                gt = GTCEu;
+            else // unsupported
+                gt = null;
+        } else
             gt = null;
-        else {
-            GT detected = null;
-            try {
-                int major = versionClass.getField("MAJOR").getInt(null);
-                int minor = versionClass.getField("MINOR").getInt(null);
-                // GTNE started with 1.18 and commits to staying on major version 1
-                if (major == 1 && minor > 17)
-                    detected = GTNE;
-                // GTCE reached EOL on version 1.17.1.770
-                else if(major <= 1)
-                    detected = GTCE;
-                else // GTCEu uses 2.0+
-                    detected = GTCEu;
-            } catch(Exception ignored) {
-            } finally {
-                gt = detected;
-            }
-        }
 
         // Get the appropriate tool class from the loaded mod, if any
+        Class<?> clazz = null;
         if(gt != null) {
             try {
                 clazz = Class.forName(gt.toolClassName, false, GTCompat.class.getClassLoader());
             } catch(Exception ignored) {
-                clazz = null;
             }
         }
         toolClass = clazz;
@@ -94,18 +80,15 @@ public class GTCompat {
         return "";
     }
 
-    /** @param m unused */
     public static boolean isGTCEu(ModContainer m) {
-        return GTCEu == gt;
+        return "GregTech".equals(m.getMetadata().name);
     }
 
-    /** @param m unused */
     public static boolean isGTCE(ModContainer m) {
-        return GTCE == gt;
+        return "GregTech Community Edition".equals(m.getMetadata().name);
     }
 
-    /** @param m unused */
     public static boolean isGTNE(ModContainer m) {
-        return GTNE == gt;
+        return "GregTech Nomifactory Edition".equals(m.getMetadata().name);
     }
 }
